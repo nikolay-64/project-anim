fece2c304573441631fbbf15daced7a16c94b976
nikolay-64 <kolyavyazmin@yandex.ru>
2026-03-10 00:43:06 +0400

    работал с fetch

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,11 +1,12 @@
 const bgElements = () => {
 	const elements = document.querySelectorAll('.set-bg');
 
-	for (let i = 0; i < elements.length; i++) {
-		const src = elements[i].dataset.setbg;
-
-		elements[i].style.backgroundImage = `url(${src})`;
-	}
-}
+	elements.forEach((element) => {
+		const bgUrl = element.dataset.setbg;
+		if (bgUrl) {
+			element.style.backgroundImage = `url(${bgUrl})`;
+		}
+	});
+};
 
 bgElements();
